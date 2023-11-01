c3e18b05715ebbfb57b0f8a1c5046b53afac40c3
Aurélie Coudert <109390102+Aurelie-Coudert@users.noreply.github.com>
2023-11-01 22:22:26 +0100

    Add files via upload

---
+++ elements/footer.php
@@ -0,0 +1,137 @@
+<!-- Bootstrap core Javascript
+Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<!-- <div class="my-5">
+ -->
+  <footer class="text-center text-lg-start text-white" id="footer">
+    <!-- Grid container -->
+    <div class="container">
+      <!--Grid row-->
+      <div class="row my-4">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+
+          <div class="shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
+            <img src="images/logo3.png" height="200" alt="logo3"
+                 loading="lazy" />
+          </div>
+
+          <p class="text-center">Ostéopathe D.O. <br> Consultations sur RDV <br> Vallet / Ancenis</p>
+
+          <ul class="list-unstyled d-flex flex-row justify-content-center">
+            <li>
+              <a class="text-white px-2" href="#!">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+            </li>
+            <li>
+              <a class="text-white px-2" href="#!">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a class="text-white ps-2" href="#!">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </li>
+          </ul>
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Plan du Site</h5>
+
+          <ul class="list-unstyled">
+            <li class="mb-2">
+              <a href="index.php" class="text-white">Accueil</a>
+            </li>
+            <li class="mb-2">
+              <a href="amelie-briand.php" class="text-white">Qui suis-je ?</a>
+            </li>
+            <li class="mb-2">
+              <a href="osteopathie.php" class="text-white">Ostéopathie</a>
+            </li>
+            <li class="mb-2">
+              <a href="consultation.php" class="text-white">Consultation</a>
+            </li>
+            <li class="mb-2">
+              <a href="honoraires.php" class="text-white">Honoraires</a>
+            </li>
+            <li class="mb-2">
+              <a href="articles.php" class="text-white">Articles</a>
+            </li>
+            <li class="mb-2">
+              <a href="contact.php" class="text-white">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Liens Utiles</h5>
+
+          <ul class="list-unstyled">
+            <li class="mb-2">
+              <a href="mentionslegales.php" class="text-white">Mentions Légales</a>
+            </li>
+            <li class="mb-2">
+              <a href="deontologie.php" class="text-white">Code de Déontologie</a>
+            </li>
+            <li class="mb-2">
+              <a href="politique-confidentialite.php" class="text-white">Politique de Confidentialité</a>
+            </li>
+<!--             <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Job</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Tenders</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Contact</a>
+            </li> -->
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Contact</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <p><a href="https://www.google.com/maps/place/31+Rue+Fran%C3%A7ois+Luneau,+44330+Vallet/@47.1624572,-1.2702254,17z/data=!3m1!4b1!4m6!3m5!1s0x48060afdc3e9ec31:0xe336025fb5d08a73!8m2!3d47.1624572!4d-1.2676505!16s%2Fg%2F11bw4qjx19?entry=ttu" target="_blank"><i class="fas fa-map-marker-alt pe-2"></i> 31 Rue François Luneau, Vallet</p>
+            </li>
+            <li>
+              <p><a href="tel:0611766285"><i class="fas fa-phone pe-2"></i> 06 11 76 62 85</a></p>
+            </li>
+            <li>
+              <p><a href="contact.php"><i class="fas fa-envelope pe-2 mb-0"></i> Formulaire de contact</a></p>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2023 Copyright : 
+      <a class="text-white" href="" target="_blank">Réalisé par Aurélie COUDERT</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+
+<!-- </div>
+ -->
---
+++ elements/header.php
@@ -0,0 +1,96 @@
+<?php
+require_once 'functions.php';
+
+/* a revoir */ // ===========================================
+// Générez un jeton CSRF unique pour la session actuelle
+//$csrfToken = bin2hex(random_bytes(32)); // Génération d'un jeton de 32 octets
+
+// Stockez le jeton CSRF dans une variable de session
+//session_start();
+//$_SESSION['csrf_token'] = $csrfToken;
+
+// Incluez le jeton CSRF dans un formulaire
+// ==========================================================
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=yes">
+    <!-- ACTIVER EN PROD <base href="//www.amelie-briand.fr"> -->
+    <meta name="robots" content="index,follow">  <!-- indique aux robots d'indexation des moteurs de recherche qu'ils sont autorisés à indexer la page en question et à suivre les liens qu'elle contient. Cela signifie que la page sera prise en compte dans les résultats de recherche, et que les liens sortants de cette page seront également explorés et pris en compte. -->
+    <!-- ACTIVER EN PROD <link rel="canonical" href="https://www.amelie-briand.fr"> -->  <!-- permet de déclarer l'URL principale ou préférée d'une page, aidant ainsi à résoudre les problèmes de contenu en double et à améliorer la visibilité dans les moteurs de recherche. -->
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <!-- ================================================================================================ -->
+    <meta name="author" content="">
+
+    <title><?php if (isset($title)) { echo $title; } else { echo 'Amélie Briand' ; } ?></title>
+    <meta property="og:type" content="website"> <!-- L'utilisation de cette balise Open Graph permet de fournir aux plateformes de médias sociaux, en particulier Facebook, des informations sur la nature du contenu de la page.  -->
+    <meta property="og:url" content="https://amelie-briand.fr">
+    <meta property="og:title" content="Ostéopathe Vallet 44300 | Amélie Briand">
+    <meta name="description" content="Ostéopathe à Vallet, Amélie Briand accueille nourrissons, enfants, adolescents, adultes, seniors, femmes enceintes et encore sportifs dans son cabinet pour soulager et soigner vos douleurs aigues et chroniques.">
+    <meta property="og:description" content="Ostéopathe à Vallet, Amélie Briand accueille nourrissons, enfants, adolescents, adultes, seniors, femmes enceintes et encore sportifs dans son cabinet pour soulager et soigner vos douleurs aigues et chroniques.">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="elements/CSS/style.css">
+
+    <script src="script.js"></script> <!-- Lien vers fichier JavaScript -->
+
+
+    <!-- a revoir
+    <meta name="csrf-param" content="authenticity_token"> sécurité pour le formulaire de contact notamment -->
+    <!-- <meta name="csrf-token"> --> 
+
+
+</head>
+
+<body>  
+ 
+<header id="header">
+    <div class="container clearfix">
+        <div id="et-top-navigation" data-height="190" data-fixed-height="30">
+            <nav class="navbar navbar-expand-lg">
+                <a class="logo" href="/index.php" onmouseover="changeLogo(true)" onmouseout="changeLogo(false)">
+                    <img src="images/logo-normal.png" alt="Logo" id="logo-img" class="logo-image">
+                </a>
+                <span class="mobile-only">Cabinet Ostéopathie - Amélie Briand</span>
+                <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <a href="https://www.doctolib.fr/osteopathe/vallet/amelie-briand" target="_blank"></a>    
+                    <span class="navbar-toggler-icon custom-toggler-icon">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                    </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <?= nav_menu('nav-link') ?>
+                    </ul>
+                </div>
+                <div id="rdv" class="d-grid gap-2">
+                    <a href="https://www.doctolib.fr/osteopathe/vallet/amelie-briand" target="_blank">
+                        <button class="btn" type="button">Prendre rendez-vous en ligne</button>
+                    </a>
+                </div>
+
+                
+            </nav>
+        </div>
+    </div>
+</header>
+
+
+</body>
+</html>
