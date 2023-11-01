7203a7349e142d1e750188416221aefedac8c1ce
Aurélie Coudert <109390102+Aurelie-Coudert@users.noreply.github.com>
2023-11-01 22:22:59 +0100

    Add files via upload

---
+++ data/config.php
@@ -0,0 +1,46 @@
+<?php
+
+/* Définir les jours */
+define('JOURS', [
+    'Lundi',
+    'Mardi',
+    'Mercredi',
+    'Jeudi',
+    'Vendredi',
+    'Samedi',
+    'Dimanche'
+]);
+
+/* Définir les créneaux horaires */
+define ('CRENEAUX', [
+    // lundi
+    [
+        [9, 13],
+        [14, 19],
+    ],
+    // mardi
+    [
+        [9, 13],
+        [14, 19],
+    ],
+    // mercredi
+    [
+        [9, 13],
+        [14, 19],
+    ],
+    // jeudi
+    [],
+    // vendredi
+    [
+        [9, 13],
+        [14, 19],
+    ],
+    // samedi
+    [
+        [9, 13],
+    ],
+    // dimanche 
+    [],
+]);
+
+?>
