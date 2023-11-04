3d37cfa34918df7ed91321426c8281b42b440254
Aurelie-Coudert <aurelie.proffit@gmail.com>
2023-11-04 22:24:41 +0100

    page politique de confidentialité texte ok

--- politique-confidentialite.php
+++ politique-confidentialite.php
@@ -7,46 +7,43 @@ require 'elements/header.php'; ?>
 
     <div class="container clearfix">
 
-        <div id="et-main-area">
-        
-            <div id="main-content">
+        <div class="bandeau-osteopathie">
+                <h3>Politique de Confidentialité</h3>
+        </div>
 
-                    <article id="post-863" class="post-863 page type-page status-publish hentry">
-                        <div class="entry-content">
-                            <div class="et-l et-l--post">
-                                <div class="et_builder_inner_content et_pb_gutters3">
-                                    <div class="et_pb_section et_pb_section_0 et_section_regular">
-                                        <div class="et_pb_row et_pb_row_0">
-                                            <div class="et_pb_column et_pb_column_4_4 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough et-last-child">
-                                                <div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_light">
-                                                    <div class="et_pb_text_inner">
-                                                        <h1>Politique de confidentialité</h1>
-                                                    </div>
-                                                </div>
-                                                <div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_light">
-                                                    <div class="et_pb_text_inner">
-                                                        <p>La protection des Données Personnelles est une préoccupation majeure pour Sophro Camino. Ainsi, la transparence de traitement des Données Personnelles que nous collectons est un engagement pour nous. La présente Charte vise à vous fournir toutes les informations pertinentes relatives au traitement des Données Personnelles qui peuvent être collectées sur ce site sophrocamino.fr</p>
-                                                        <h2>Traitement de vos données personnelles :</h2>
-                                                            <p>De manière générale, vous pouvez visiter notre site Internet sans fournir de Données Personnelles vous concernant. Toutefois, pour accéder à certaines parties de nos sites Internet et/ou dans le cadre d’une utilisation de l’information spécifique ou des services, nous pouvons être amenés à collecter vos Données Personnelles que nous traiterons pour les finalités mentionnées ci-après.<br>Sophro Camino peut collecter et traiter des Données Personnelles vous concernant lorsque vous visitez ce site, à l’occasion des demandes de contact, d’information, de rappel ou de candidature.<br>Les données ne seront jamais utilisées à des fins autres, non prévues, ni transmises à des partenaires, sauf accord de la part du propriétaire des données. Aucune information personnelle autre que celles demandées dans ce cadre n’est collectée à l’insu des personnes concernées.<br>Il est aimablement rappelé aux utilisateurs du site que l’exécution d’un contrat de vente est conditionnée à la fourniture des données à caractère personnel. L’utilisateur souhaitant conclure un contrat de vente avec le site sophrocamino.fr doit être conscient que la non-fourniture de ces données entraîne l’impossibilité de poursuivre le processus contractuel.</p>
-                                                        <h2>Conservation des Données Personnelles :</h2>
-                                                            <p>En outre, les données recueillies sont conservées :<br>• Pour les clients : pendant tout le temps de la relation commerciale. Elles seront conservées à des fins de prospection commerciale au maximum pendant 3 ans à compter de la fin de cette relation commerciale,<br>• Pour les prospects non clients : 3 ans maximum à compter de leur collecte par le responsable de traitement ou du dernier contact émanant du prospect.</p>
-                                                            <p>• Pour les candidats : 3 ans maximum à compter de leur collecte par le responsable de traitement ou du dernier contact émanant du candidat.</p>
-                                                            <p>Par ailleurs, les données collectées via les traitements de données à caractère personnel du présent site ne font l’objet d’aucun transfert en dehors du territoire de l’Union Européenne.<br>Sécurité et intégrité des données :<br>Sophro Camino utilise diverses mesures de sécurité techniques et organisationnelles afin de protéger les Données Personnelles que nous collectons, utilisons ou transférons contre les pertes, utilisations abusives, falsifications ou destructions.<br>Également, les données transitant par le site font l’objet d’une attention de tous les instants quant à leur sécurité et leur confidentialité. L’accès à l’arrière-guichet (back-office) du site est possible via des logins et des mots de passe complexes à protection forte ainsi que d’une protection avec double authentification. Il est en de même pour la boîte mail réceptacle des données personnelles des utilisateurs et Sophro Camino.<br>Également, le site web sophrocamino.fr est équipé de nombreux modules de sécurité empêchant au maximum les intrusions malveillantes et les fuites de données.<br>Cependant, l’utilisateur de ce site ou Sophro Camino peut à tout moment introduire une réclamation auprès de la Commission Nationale Informatique et Libertés (CNIL), autorité de contrôle française.</p>
-                                                        
-                                                        <h2>Vos droits :</h2>
-                                                            <p>Enfin, conformément aux articles 15 et suivants du Règlement général sur la protection des données, l’utilisateur de ce site bénéficie des droits d’accès, de rectification, d’effacement et de limitation portant sur les données le concernant ainsi que du droit d’opposition portant sur les traitements de données opérés via le site sophrocamino.fr</p>
-                                                            <h2>Sites Internet extérieurs :</h2>
-                                                            <p>Les sites Internet deSophro Camino peuvent fournir des liens vers des sites Internet extérieurs. Sophro Camino ne fournit aucune garantie ni ne fait aucune déclaration relative à ces sites Internet extérieurs. Il est important que vous sachiez que les propriétaires et opérateurs de ces sites Internet extérieurs peuvent collecter, utiliser ou transférer des Données Personnelles dans le cadre de termes et conditions différents de ceux de Sophro Camino. En vous connectant à un site Internet extérieur, vous devez vous informer sur les mesures de confidentialité et de sécurité de ces sites Internet extérieurs.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>		
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-            </div>
+        <div class="body-page-osteopathie">
+
+            <p>Au Cabinet La Source, nous accordons une importance primordiale à la confidentialité et à la sécurité de vos informations personnelles. 
+            Cette politique de confidentialité explique comment nous collectons, utilisons, partageons et protégeons vos données lorsque vous visitez 
+            notre site web ou utilisez nos services.</p>
+
+            <p><strong>1. Collecte de données personnelles</strong></p>
+            <p>Nous pouvons collecter des informations personnelles lorsque vous utilisez notre site web ou nos services, telles que votre nom, adresse e-mail, 
+            numéro de téléphone, antécédents médicaux et d'autres données liées à votre santé. Ces informations sont collectées uniquement lorsque vous nous 
+            les fournissez volontairement, par exemple, en remplissant un formulaire de contact ou en prenant rendez-vous.</p>
+            
+            <p><strong>2. Utilisation des données</strong></p>
+            <p>Nous utilisons vos données personnelles dans le but de fournir nos services d'ostéopathie et de vous contacter pour des rappels de rendez-vous, 
+            des informations pertinentes sur votre traitement, ou des informations sur nos services. Nous ne vendons ni ne louons vos informations personnelles à des tiers.</p>
+            
+            <p><strong>3. Sécurité des données</strong></p>
+            <p>Nous prenons des mesures de sécurité appropriées pour protéger vos données personnelles contre tout accès non autorisé, toute divulgation ou destruction. 
+            Vos informations sont stockées dans des environnements sécurisés et accessibles uniquement aux personnes autorisées.</p>
+
+            <p><strong>4. Cookies et technologies similaires</strong></p>
+            <p>Nous utilisons des cookies et d'autres technologies de suivi pour améliorer votre expérience sur notre site web. Vous pouvez gérer les préférences de 
+            cookies en ajustant les paramètres de votre navigateur.</p>
+
+            <p><strong>5. Vos droits</strong></p>
+            <p>Vous avez le droit d'accéder à vos données personnelles, de les corriger, de les supprimer, ou de vous opposer à leur traitement. Pour exercer ces droits, 
+            veuillez nous contacter via notre formulaire de contact.</p>
+
+            <p><strong>6. Modifications de la politique de confidentialité</strong></p>
+            <p>Nous pouvons mettre à jour cette politique de confidentialité de temps à autre pour refléter les changements dans nos pratiques de collecte et d'utilisation des données. 
+            Les modifications seront affichées sur cette page.<br>En utilisant notre site web et nos services, vous consentez à notre politique de confidentialité. Si vous avez des questions 
+            concernant cette politique ou nos pratiques en matière de confidentialité, veuillez nous contacter via notre formulaire de contact.<br>Merci de faire confiance au Cabinet de la Source 
+            pour votre bien-être. Nous sommes déterminés à protéger vos informations personnelles et à vous offrir des soins de haute qualité.</p>
+
         </div>
     </div>
 </div>
